15afda9b4bc2f7281372f553a5eb90695f23c06d
Brock Wilcox <awwaiid@thelackthereof.org>
2018-07-22 14:07:08 -0400

    Add basic simulator interface

---
+++ lib/Simulator.pm
@@ -0,0 +1,30 @@
+use Moops;
+
+class Simulator {
+  use JSON::XS;
+  use IPC::Open2;
+
+  has to_sim => (is => 'rw' );
+  has from_sim => (is => 'rw' );
+
+  method BUILD($args) {
+    $self->connect( $args->{resolution} || 20 );
+  }
+
+  method connect($resolution) {
+    my ($to_sim, $from_sim);
+    open2($from_sim, $to_sim, "./simulator/simulator.native $resolution");
+    $self->to_sim($to_sim);
+    $self->from_sim($from_sim);
+  }
+
+  method send($data) {
+    $self->to_sim->print(encode_json($data));
+    my $response = $self->from_sim->getline;
+    return decode_json($response);
+  }
+
+  method state {
+    return $self->send({cmd: "state"});
+  }
+}
